1bf32e024eca5de0c778d74f94454f01b8578d42
AlexCrt64 <alexandre.courtet@epitech.digital>
2025-09-25 14:44:53 +0200

    [ADD] Add multiple pages for accompte

--- Smartcity/app/Login.tsx
+++ Smartcity/app/Login.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
+import { navigate } from 'expo-router/build/global-state/routing'
 
 const Login = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <View style={styles.buttonConnect}>
+        <Button onPress={() => navigate('/Home')} title="Connexion" color="#666FAB" accessibilityLabel="Bouton pour valider sa connexion" />
+      </View>
     </View>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    buttonConnect:{
+        backgroundColor: "#36418F",
+        borderRadius: 10,
+        fontFamily: 'Manrope',
+        marginBottom: 18,
+        width: 353,
+        height: 56,
+        justifyContent: 'center'
+    }
+}
+)
